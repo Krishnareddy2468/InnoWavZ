83f29f3294d17562d0c891228f053ecf15c5492a
krishna reddy <kittureddy2468@gmail.com>
2025-03-17 21:50:51 +0530

    ready for render

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
