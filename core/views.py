6006fb597ac665fd520fde4aa67a6eb01911bbf5
krishna reddy <kittureddy2468@gmail.com>
2025-03-17 23:57:02 +0530

    deploy3

--- core/views.py
+++ core/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render 
 
 def index(request):
-    return render(request, 'core/index.html')  # Corrected path
+    return render(request, 'InnoWavZ/templates/core/index.html')  # Corrected path
 def project(request):
-    return render(request, 'core/project.html')  # Corrected path
+    return render(request, 'InnoWavZ/templates/core/project.html')  # Corrected path
 def about(request):
-    return render(request, 'core/about.html')  # Corrected
+    return render(request, 'InnoWavZ/templates/core/about.html')  # Corrected
 def contact(request):
-    return render(request, 'core/contact.html')  # Corrected
+    return render(request, 'InnoWavZ/templates/core/contact.html')  # Corrected
 def services(request):
-    return render(request, 'core/service.html')  # Corrected
+    return render(request, 'InnoWavZ/templates/core/service.html')  # Corrected
